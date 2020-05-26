973887c05111175774f11ac0660117e6700de870
gaobo <278708579@qq.com>
2020-05-26 12:07:22 +0800

    update

---
+++ EventBus/src/EventBus/IEventHandlerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventBus
+{
+    public interface IEventHandlerFactory
+    {
+        ICollection<IIntegrationEventHandler> GetHandlers(Type eventType);
+    }
+
+    public class EventHandlerFactory : IEventHandlerFactory
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public EventHandlerFactory(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+        public ICollection<IIntegrationEventHandler> GetHandlers(Type eventType)
+        {
+            var eventHandlerType = typeof(IIntegrationEventHandler<>).MakeGenericType(eventType);
+            return _serviceScopeFactory
+                .CreateScope()
+                .ServiceProvider
+                .GetServices(eventHandlerType)
+                .Cast<IIntegrationEventHandler>()
+                .ToArray();
+        }
+    }
+}
